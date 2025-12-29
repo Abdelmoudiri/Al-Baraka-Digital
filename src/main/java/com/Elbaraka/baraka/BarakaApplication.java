09fc37e3d5d0960bfb6f97b586590881d227e2e3
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-29 11:27:14 +0100

    feat: Generate and print JWT secret key on application startup

--- src/main/java/com/Elbaraka/baraka/BarakaApplication.java
+++ src/main/java/com/Elbaraka/baraka/BarakaApplication.java
@@ -1,12 +1,19 @@
 package com.Elbaraka.baraka;
 
+import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Base64;
 
 @SpringBootApplication
 public class BarakaApplication {
 
 	public static void main(String[] args) {
+
+	byte[] key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256).getEncoded();
+				System.out.println(Base64.getEncoder().encodeToString(key));
+
 		SpringApplication.run(BarakaApplication.class, args);
 	}
 
