ff359e6f0a028478b795896511d3254683dc226b
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-29 11:29:05 +0100

    feat: Add method to find User by email with roles and permissions

--- src/main/java/com/Elbaraka/baraka/repository/UserRepository.java
+++ src/main/java/com/Elbaraka/baraka/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.Elbaraka.baraka.repository;
 
 import com.Elbaraka.baraka.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,4 +12,7 @@ public interface UserRepository extends JpaRepository<User,Long> {
     Boolean existsByEmail(String email);
 
     Optional<User>  findByEmail(String email);
+    
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles r LEFT JOIN FETCH r.permissions WHERE u.email = :email")
+    Optional<User> findByEmailWithRoles(@Param("email") String email);
 }
