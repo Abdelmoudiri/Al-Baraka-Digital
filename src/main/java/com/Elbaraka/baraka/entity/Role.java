66bb1f41d07cb56144908afa9c47ea1526bf831b
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-29 11:26:04 +0100

    feat: Add Role entity with permissions mapping and attributes

---
+++ src/main/java/com/Elbaraka/baraka/entity/Role.java
@@ -0,0 +1,34 @@
+package com.Elbaraka.baraka.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "roles_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions;
+}
