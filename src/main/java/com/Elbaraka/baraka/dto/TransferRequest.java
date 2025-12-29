49ac9029979eeec5f31b0a50b27f484c00dab39b
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-29 11:28:11 +0100

    feat: Add TransferRequest class for fund transfer data transfer

---
+++ src/main/java/com/Elbaraka/baraka/dto/TransferRequest.java
@@ -0,0 +1,23 @@
+package com.Elbaraka.baraka.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransferRequest {
+
+    @NotBlank(message = "Le numéro de compte de destination est obligatoire")
+    private String destinationAccountNumber;
+
+    @NotNull(message = "Le montant est obligatoire")
+    @DecimalMin(value = "0.01", message = "Le montant doit être supérieur à 0")
+    private BigDecimal amount;
+}
