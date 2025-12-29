546954eb2f1f474a82dcfcdeab806438a1c79d35
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-29 11:28:49 +0100

    feat: Add methods to find Operations by account source/destination and by status

--- src/main/java/com/Elbaraka/baraka/repository/OperationRepository.java
+++ src/main/java/com/Elbaraka/baraka/repository/OperationRepository.java
@@ -1,7 +1,13 @@
 package com.Elbaraka.baraka.repository;
 
+import com.Elbaraka.baraka.entity.Account;
 import com.Elbaraka.baraka.entity.Operation;
+import com.Elbaraka.baraka.enums.OperationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OperationRepository extends JpaRepository<Operation,Long> {
+    List<Operation> findByAccountSourceOrAccountDestinationOrderByCreatedAtDesc(Account accountSource, Account accountDestination);
+    List<Operation> findByStatusOrderByCreatedAtDesc(OperationStatus status);
 }
