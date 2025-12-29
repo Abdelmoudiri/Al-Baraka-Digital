60d704690e193c0f114c8d8c08f403e6bb152c2a
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-29 11:27:55 +0100

    feat: Create OperationRequest class for operation data transfer

---
+++ src/main/java/com/Elbaraka/baraka/dto/OperationRequest.java
@@ -0,0 +1,20 @@
+package com.Elbaraka.baraka.dto;
+
+import com.Elbaraka.baraka.enums.OperationType;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OperationRequest {
+
+    @NotNull(message = "Le montant est obligatoire")
+    @DecimalMin(value = "0.01", message = "Le montant doit être supérieur à 0")
+    private BigDecimal amount;
+}
