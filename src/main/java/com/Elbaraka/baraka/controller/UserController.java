b7f7c468e1459a17b45356822ecba12c053b8f2d
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-29 11:30:59 +0100

    feat: Refactor UserController to improve user management endpoints

--- src/main/java/com/Elbaraka/baraka/controller/UserController.java
+++ src/main/java/com/Elbaraka/baraka/controller/UserController.java
@@ -3,11 +3,11 @@ package com.Elbaraka.baraka.controller;
 import com.Elbaraka.baraka.entity.User;
 import com.Elbaraka.baraka.service.UserService;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,11 +22,30 @@ public class UserController {
         this.userService=u;
     }
 
+
+    //@PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAll()
     {
         return ResponseEntity.ok(userService.getAllUsers());
-//        List<User> all=userService.getAllUsers();
-//        return ResponseEntity.status(HttpStatus.OK).body(all);
+    }
+
+    @PutMapping("users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user)
+    {
+        return ResponseEntity.ok(userService.updateUser(id,user));
+
+    }
+
+    @GetMapping("users/{id}")
+    public User getUser(@PathVariable Long id)
+    {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("admin/user")
+    public ResponseEntity<User> creatUser(@Valid @RequestBody User user)
+    {
+        return ResponseEntity.ok(userService.createUser(user));
     }
 }
