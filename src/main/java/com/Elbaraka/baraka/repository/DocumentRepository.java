cc611ac95d796aea16575df4311c4679e0ef312a
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-29 11:28:44 +0100

    feat: Add methods to find and count Documents by operation ID

--- src/main/java/com/Elbaraka/baraka/repository/DocumentRepository.java
+++ src/main/java/com/Elbaraka/baraka/repository/DocumentRepository.java
@@ -3,5 +3,9 @@ package com.Elbaraka.baraka.repository;
 import com.Elbaraka.baraka.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DocumentRepository extends JpaRepository<Document,Long> {
+    List<Document> findByOperationId(Long operationId);
+    long countByOperationId(Long operationId);
 }
