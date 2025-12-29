5c12d160c964f3f643c3d4324b28fde346eeada1
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-29 11:29:42 +0100

    feat: Implement UserDetailsService to load user details by email with roles

---
+++ src/main/java/com/Elbaraka/baraka/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.Elbaraka.baraka.service.impl;
+
+import com.Elbaraka.baraka.entity.User;
+import com.Elbaraka.baraka.repository.UserRepository;
+import com.Elbaraka.baraka.security.UserDetailsImpl;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmailWithRoles(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+        
+        return new UserDetailsImpl(user);
+    }
+}
